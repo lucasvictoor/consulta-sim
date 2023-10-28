6793febec49073ac3a84bf76dbfcb3025743c173
lucas8790 <lv8790121@gmail.com>
2023-10-28 20:45:48 -0300

    Aplicando validations para medico e paciente

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -2,4 +2,9 @@ class Medico < ApplicationRecord
   has_many :consultas, foreign_key: 'medico_id'
   attr_accessor :especializacao
   attr_accessor :nome, :cpf, :email, :crm
+
+  #Validations
+  validates :especialidade, presence: true, length: { maximum: 255 }
+  validates :crm, presence: true, uniqueness: true, format: { with: /\A\d{7}\z/, message: 'Obrigatório 7 dígitos numéricos' }
+  validates :cpf, presence: true, format: { with: /\A\d{11}\z/, message: 'Obrigatório 11 dígitos numéricos' }
 end
--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,5 +1,10 @@
 class Paciente < ApplicationRecord
   has_one :endereco
   has_many :consultas
+
+  #Validations
+  validates :nome_completo, presence: true, length: { maximum: 255 }
+  validates :cpf, presence: true, format: { with: /\A\d{11}\z/, message: 'Obrigátorio ter 11 dígitos numéricos' }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
 
