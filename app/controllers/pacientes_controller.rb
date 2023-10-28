acbf668aec6e151fa1d5346eab326514c18c5016
lucas8790 <lv8790121@gmail.com>
2023-10-28 18:23:37 -0300

    Criando views do paciente                             formulário para preencher as informações

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -1,2 +1,49 @@
 class PacientesController < ApplicationController
+  before_action :set_paciente, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pacientes = Paciente.all
+  end
+
+  def show
+  end
+
+  def new
+    @paciente = Paciente.new
+  end
+
+  def create
+    @paciente = Paciente.new(paciente_params)
+    if @paciente.save
+      redirect_to @paciente, notice: 'Paciente criado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @paciente.update(paciente_params)
+      redirect_to @paciente, notice: 'Paciente atualizado com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @paciente.destroy
+    redirect_to pacientes_url, notice: 'Paciente excluído com sucesso.'
+  end
+
+  private
+
+  def set_paciente
+    @paciente = Paciente.find(params[:id])
+  end
+
+  def paciente_params
+    params.require(:paciente).permit(:nome_completo, :data_nascimento, :cpf, :email)
+  end
 end
