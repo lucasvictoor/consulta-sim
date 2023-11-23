bed2e506206d2b3fff0204008a2c630442e6885c
lucas8790 <lv8790121@gmail.com>
2023-11-23 11:17:08 -0300

    Correção datas agendadas no passado

--- app/models/consulta.rb
+++ app/models/consulta.rb
@@ -3,4 +3,13 @@ class Consulta < ApplicationRecord
   belongs_to :medico
 
   validates :data, presence: true
+  validate :data_nao_pode_ser_no_passado
+
+  private
+
+  def data_nao_pode_ser_no_passado
+    if data.present? && data < Date.today
+      errors.add(:data, "não pode ser no passado")
+    end
+  end
 end
